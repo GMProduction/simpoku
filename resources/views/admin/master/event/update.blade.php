7b48ef0bdf77367ba37188d339a2ec83ec54d72d
bagusyanuar <bagus.yanuar613@gmail.com>
2019-10-11 21:40:46 +0700

    s

--- resources/views/admin/master/event/update.blade.php
+++ resources/views/admin/master/event/update.blade.php
@@ -259,4 +259,10 @@ Edit Event
         });
 }
 </script>    
+<script>
+    $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
+</script>
 @endsection
