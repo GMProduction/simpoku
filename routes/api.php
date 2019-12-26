e51ff9f0466f05fd4f10a994246382363cbb2422
pradanna <pradanamahendra@gmail.com>
2019-12-26 14:32:44 +0700

    1

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('v1/dataEvent', 'api\EventController@dataEvent')->name('dataEvent');
+Route::get('v1/dataEvent', 'api\eventController@dataEvent')->name('dataEvent');
